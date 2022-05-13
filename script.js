2cb26fe4714ef911701b542607ece7b8d9951105
piesieomane <piesieyeboah@gmail.com>
2022-05-14 01:16:28 +0300

    Second commit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-var icon = document.querySelector("menu-pic");
-var sidebar = document.querySelector("sidebar");
+var icon = document.querySelector(".menu-pic");
+var sidebar = document.querySelector(".sidebar");
+// var videos = document.querySelector(".videos");
 
-functionToExecute() {
-    sidebar.classList.toggle(".menu-sidebar")
-}  
+
+icon.onclick = function(){
+    sidebar.classList.toggle("menu-sidebar");
+    // videos.classList.toggle("video-extend");
+};
